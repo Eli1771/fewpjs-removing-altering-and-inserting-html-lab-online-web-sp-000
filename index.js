50edc3ecadbb7c2efff0f5241a6546df5fe911b8
elijahmm17 <elijahmm17@gmail.com>
2019-10-02 20:37:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 // Write your code here!
 
-document.querySelector('main#main').remove();
-
-let newHeader = document.querySelector('h1#victory');
-
-newHeader.innerHTML = "Eli is the champion";const testVar = "hi"
-
 document.querySelector("main#main").remove();
 
 const newHeader = document.createElement("h1");
